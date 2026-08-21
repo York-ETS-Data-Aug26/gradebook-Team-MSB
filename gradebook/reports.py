e8f2e296b773f7e7aaf33bd98de5bafb4fae210a
Sydney Porter <SPorterC@Yorksolutions.net>
2026-08-21 12:59:47 -0400

    Reversing Changes

--- gradebook/reports.py
+++ gradebook/reports.py
@@ -6,7 +6,7 @@ from gradebook.errors import ReportError
 def mean(scores):
     #We use an if statement becasue we know this is a bad/frequent condition that can happen. So we check for it directly.
     #Try/except will just already attempt the operation and react if Python throws an exception.
-    if len(scores) == 1:
+    if len(scores) == 0:
         raise ReportError("Cannot calculate the mean of an empty list!!!") #When raised means that something went wrong. Throw an exception.
     #If good then run as normal.
     return sum(scores) / len(scores)
@@ -14,7 +14,7 @@ def mean(scores):
 def summary(roster):
     return f'''
     --=Student Report=--
-    Name: Hello
+    Name: 
     Score:
     '''
 
