6f4e0883bd7bd63b239e9e75784388439c7c9532
Sydney Porter <SPorterC@Yorksolutions.net>
2026-08-21 14:04:22 -0400

    Second change on line 9

--- gradebook/reports.py
+++ gradebook/reports.py
@@ -6,7 +6,7 @@ from gradebook.errors import ReportError
 def mean(scores):
     #We use an if statement becasue we know this is a bad/frequent condition that can happen. So we check for it directly.
     #Try/except will just already attempt the operation and react if Python throws an exception.
-    if len(scores) == 0:
+    if len(scores) == 2:
         raise ReportError("Cannot calculate the mean of an empty list!!!") #When raised means that something went wrong. Throw an exception.
     #If good then run as normal.
     return sum(scores) / len(scores)
